855d7a5595e1a065d0b4878b9999350521e1c2bd
panda <a17877yun@gmail.com>
2013-11-13 15:57:57 +0800

    add update devise code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -18,8 +18,29 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    #xx = registration_params
+    #yy = account_update_params
+
+    if update_resource(resource, registration_params)
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+            :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
   def registration_params
-    params.require(:user).permit(:name,:department,:phone,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:department,:phone,:email,:password,:password_confirmation,:current_password)
   end
 
 end
